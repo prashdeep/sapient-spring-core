18b8602350b46369ce8bc6111ee484e4caa2764f
pradeep <pradeep.kumar44@gmail.com>
2019-07-15 10:18:23 +0530

    committing with setters and getters

--- src/main/java/com/sapient/springdemo/model/Employee.java
+++ src/main/java/com/sapient/springdemo/model/Employee.java
@@ -8,4 +8,33 @@ public class Employee {
     private final String name;
     private  int age;
     private double salary;
+
+    public Employee(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
