dd8dd600ca59fb3e3a01fc541e30e80e0287cee4
pradeep <pradeep.kumar44@gmail.com>
2019-07-15 14:13:16 +0530

    committing the changes after aop example

---
+++ src/main/java/com/sapient/springdemo/aspects/LoggingAspect.java
@@ -0,0 +1,17 @@
+package com.sapient.springdemo.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    @Before("execution(* com.sapient.springdemo.service.EmployeeServiceImpl.saveEmployee(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("logging before the method is getting executed");
+        System.out.println(joinPoint);
+    }
+}
--- src/main/java/com/sapient/springdemo/client/EmployeeCRUDClient.java
+++ src/main/java/com/sapient/springdemo/client/EmployeeCRUDClient.java
@@ -1,19 +1,17 @@
 package com.sapient.springdemo.client;
 
-import com.sapient.springdemo.config.AppConfig;
 import com.sapient.springdemo.model.Employee;
 import com.sapient.springdemo.service.EmployeeService;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class EmployeeCRUDClient {
 
     public static void main(String[] args) {
-        //ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
         //Java configuration
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        EmployeeService employeeService = context.getBean(EmployeeService.class);
+        //ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        EmployeeService employeeService = context.getBean("employeeService" ,EmployeeService.class);
         Employee pavan = new Employee(23, "Pavan");
         employeeService.saveEmployee(pavan);
     }
