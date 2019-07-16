5cbfcff3a0ff83690612023aa76635cc233ba4a3
pradeep <pradeep.kumar44@gmail.com>
2019-07-16 08:16:05 +0530

    committing the jdbcimplementation

--- src/main/java/com/sapient/springdemo/aspects/LoggingAspect.java
+++ src/main/java/com/sapient/springdemo/aspects/LoggingAspect.java
@@ -1,7 +1,10 @@
 package com.sapient.springdemo.aspects;
 
+import com.sapient.springdemo.model.Employee;
 import com.sapient.springdemo.service.EmployeeService;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -10,10 +13,23 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(* com.sapient.springdemo.service.EmployeeServiceImpl.saveEmployee(..))")
+    @Before("execution(* com.sapient.springdemo.service.EmployeeServiceImpl.*(..))")
     public void logBefore(JoinPoint target) {
         System.out.println("------------logging before the method is getting executed-------------------");
         System.out.println(target.getArgs()[0]);
         System.out.println(target.getTarget() instanceof EmployeeService);
+    }
+
+    @Around("execution(* com.sapient.springdemo.service.EmployeeServiceImpl.*(..))")
+    public void logAround(ProceedingJoinPoint joinPoint) {
+        System.out.println("------------logging before the method is getting executed-------------------");
+        if(joinPoint.getArgs()[0] instanceof Employee){
+            try {
+                joinPoint.proceed();
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        }
+        System.out.println("-------------------Logging after the successfull execution of the method -----------------");
     }
 }
--- src/main/java/com/sapient/springdemo/dao/MySQLEmployeeDAOImpl.java
+++ src/main/java/com/sapient/springdemo/dao/MySQLEmployeeDAOImpl.java
@@ -1,15 +1,29 @@
 package com.sapient.springdemo.dao;
 
 import com.sapient.springdemo.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository(value = "mysqlDAOImpl")
 public class MySQLEmployeeDAOImpl implements EmployeeDAO {
 
+    private JdbcTemplate jdbcTemplate;
+
+    public MySQLEmployeeDAOImpl(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public Employee save(Employee employee) {
         System.out.println("Came inside the save method of MySQLEmployeeDAOImpl");
+       jdbcTemplate.execute("insert into employee(name) values ('"+employee.getName()+"')");
+
         return null;
     }
 
